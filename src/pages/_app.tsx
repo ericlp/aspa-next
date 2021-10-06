e409f067eb1e9f92261a09f019ac90732103d6c2
Eric Carlsson <cirecirecire3@gmail.com>
2021-10-06 13:32:08 +0200

    set up nextjs env

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import {Button} from "../components/Button/Button";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Button></
+      Button>//<Component {...pageProps} />
+}
+export default MyApp
+
+//https://codesandbox.io/s/responsive-sticky-header-on-scroll-ushji
